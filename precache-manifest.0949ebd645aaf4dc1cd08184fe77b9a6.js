0ed3d8a1810bf3a9807e10a339127add22539552
yutsu <puremaaath@gmail.com>
2019-07-28 22:13:34 -0400

    Updates

---
+++ precache-manifest.0949ebd645aaf4dc1cd08184fe77b9a6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38323663eb429444fa4fa0fef8f3813",
+    "url": "/werewolves-game-master/index.html"
+  },
+  {
+    "revision": "834e0a5346cf32d16bec",
+    "url": "/werewolves-game-master/static/js/2.000d1c15.chunk.js"
+  },
+  {
+    "revision": "2412c9de255f72a2224e",
+    "url": "/werewolves-game-master/static/js/main.91d9b90f.chunk.js"
+  },
+  {
+    "revision": "5577dd032a0eec1c4170",
+    "url": "/werewolves-game-master/static/js/runtime~main.68d5dddd.js"
+  }
+]);
